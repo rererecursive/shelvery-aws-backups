402378536e6e124f380c4a6f69ae12c44e173f06
rererecursive <z.lewis@base2services.com>
2018-08-03 15:17:18 +1000

    Add function and more logic to determine backup name.

--- shelvery/backup_resource.py
+++ shelvery/backup_resource.py
@@ -10,25 +10,25 @@ from shelvery.runtime_config import RuntimeConfig
 
 class BackupResource:
     """Model representing single backup"""
-    
+
     BACKUP_MARKER_TAG = 'backup'
+    MAXIMUM_STRING_LENGTH = 127
     TIMESTAMP_FORMAT = '%Y-%m-%d-%H%M'
     TIMESTAMP_FORMAT_LEGACY = '%Y%m%d-%H%M'
-    
+
     RETENTION_DAILY = 'daily'
     RETENTION_WEEKLY = 'weekly'
     RETENTION_MONTHLY = 'monthly'
     RETENTION_YEARLY = 'yearly'
-    
+
     def __init__(self, tag_prefix, entity_resource: EntityResource, construct=False):
         """Construct new backup resource out of entity resource (e.g. ebs volume)."""
         # if object manually created
         if construct:
             return
-        
-        # current date
+
         self.date_created = datetime.utcnow()
-        
+
         # determine retention period
         if self.date_created.day == 1:
             if self.date_created.month == 1:
@@ -39,16 +39,15 @@ class BackupResource:
             self.retention_type = self.RETENTION_WEEKLY
         else:
             self.retention_type = self.RETENTION_DAILY
-        
-        # determine backup name
-        name = entity_resource.tags['Name'] if 'Name' in entity_resource.tags else entity_resource.resource_id
+
         date_formatted = self.date_created.strftime(self.TIMESTAMP_FORMAT)
-        self.name = f"{name}-{date_formatted}-{self.retention_type}"
+
+        self.name = self.determine_backup_name(date_formatted, entity_resource, self.retention_type)
 
         self.entity_id = entity_resource.resource_id
         self.entity_resource = entity_resource
         self.__region = entity_resource.resource_region
-    
+
         self.tags = {
             'Name': self.name,
             "shelvery:tag_name": tag_prefix,
@@ -61,9 +60,27 @@ class BackupResource:
         }
         self.backup_id = None
         self.expire_date = None
-        
 
-    
+    def determine_backup_name(self, date, entity, retention_type):
+        """Determine the backup name. Make it meaningful (using tags) and unique (from its ID).
+        If the name is more than 127 characters long, reduce it.
+        """
+        if 'Name' in entity.tags:
+            name = f"{entity.tags['Name']}-{entity.resource_id}"
+            name_length = len(f"{name}-{date}-{retention_type}")
+
+            if name_length > self.MAXIMUM_STRING_LENGTH:
+                # If the backup name is too long, reduce the characters from the tag.
+                print(f"The backup name is larger than {self.MAXIMUM_STRING_LENGTH}. Reducing to 127 characters...")
+                to_trim = name_length - self.MAXIMUM_STRING_LENGTH
+                name = entity.tags['Name'][:-to_trim] + '-' + entity.resource_id
+        else:
+            name = entity.resource_id
+
+        return f"{name}-{date}-{retention_type}"
+
+
+
     @classmethod
     def construct(cls,
                   tag_prefix: str,
@@ -72,20 +89,20 @@ class BackupResource:
         """
         Construct BackupResource object from object id and aws tags stored by shelvery
         """
-        
+
         obj = BackupResource(None, None, True)
         obj.entity_resource = None
         obj.entity_id = None
         obj.backup_id = backup_id
         obj.tags = tags
-        
+
         # read properties from tags
         obj.retention_type = tags[f"{tag_prefix}:retention_type"]
         obj.name = tags[f"{tag_prefix}:name"]
-        
+
         if f"{tag_prefix}:entity_id" in tags:
             obj.entity_id = tags[f"{tag_prefix}:entity_id"]
-            
+
         try:
             obj.date_created = datetime.strptime(tags[f"{tag_prefix}:date_created"], cls.TIMESTAMP_FORMAT)
         except Exception as e:
@@ -93,8 +110,8 @@ class BackupResource:
                 str_date = tags[f"{tag_prefix}:date_created"]
                 print(f"Failed to read {str_date} as date, trying legacy format {cls.TIMESTAMP_FORMAT_LEGACY}")
                 obj.date_created = datetime.strptime(tags[f"{tag_prefix}:date_created"], cls.TIMESTAMP_FORMAT_LEGACY)
-                
-                
+
+
         obj.region = tags[f"{tag_prefix}:region"]
         return obj
 
@@ -118,18 +135,18 @@ class BackupResource:
         else:
             # in case there is no retention tag on backup, we want it kept forever
             expire_date = datetime.utcnow() + relativedelta(years=10)
-        
+
         self.expire_date = expire_date
-    
+
     def is_stale(self, engine):
         self.calculate_expire_date(engine)
         now = datetime.now(self.date_created.tzinfo)
         return now > self.expire_date
-    
+
     @property
     def region(self):
         return self.__region
-    
+
     @region.setter
     def region(self, region: str):
         self.__region = region
